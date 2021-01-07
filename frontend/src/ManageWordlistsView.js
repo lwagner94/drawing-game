f8d7a5c8628e647f637f33b4d2d465277f893125
Christoph Ladler <christoph.ladler@student.tugraz.at>
2021-01-07 18:44:37 +0100

    finished drag and drop and storing it into sessionStorage

--- frontend/src/ManageWordlistsView.js
+++ frontend/src/ManageWordlistsView.js
@@ -10,6 +10,8 @@ export default class ManageWordlistsView extends AbstractView {
 
         this.wordlists = document.getElementById("wordlists_currently");
 
+        loadSessionStorageWordlists(this.wordlists);
+
         document.querySelectorAll(".drop_zone__input").forEach(inputElement => {
             const dropZoneElement = inputElement.closest(".drop_zone");
 
@@ -41,14 +43,62 @@ export default class ManageWordlistsView extends AbstractView {
 }
 
 function updateWordlists(wordlistsElement, files) {
-    console.log(files);
     
     Array.from(files).forEach(file => {
-        console.log(file.name);
-    
-        // TODO: read file content into Webstorage
-
-        let file_name = file.name.split(".");
-        wordlistsElement.innerHTML += ('<li>' + file_name[0] + '<\li>');
+        
+        let file_name = file.name.split(".")[0];
+        addWordlist(wordlistsElement, file_name);
+        
+        readFile(file).then((content) => {
+            storeWordlist(file_name, content);
+        });
     });
+}
+
+function readFile(file) {
+    const reader = new FileReader();
+
+    return new Promise((resolve, reject) => {
+        reader.onload = event => resolve(event.target.result);
+        reader.onerror = event => reject(error);
+        reader.readAsText(file);
+    });
+}
+
+function storeWordlist(wordlist_name, wordlist_content) {
+    
+    if (typeof(Storage) !== undefined) {
+        let wordlist = {};
+        wordlist.title = wordlist_name;
+        wordlist.words = wordlist_content.split(",");
+        
+        for (let i = 0; i < wordlist.words.length; i++) {
+            wordlist.words[i] = wordlist.words[i].trim().toLowerCase();
+        }
+
+
+        // TODO: sanity checks?
+        let sessionStorage = window.sessionStorage;
+        sessionStorage.setItem(wordlist_name, JSON.stringify(wordlist));
+    }
+    else {
+        // TODO: error handling
+        console.log("Webstorage not supported.");
+    }
+}
+
+function loadSessionStorageWordlists(wordlistsElement) {
+    const storage = window.sessionStorage;
+
+    if (storage.length) {
+        for (let i = 0; i < storage.length; i++) {
+            
+            let wordlist_name = storage.key(i);
+            addWordlist(wordlistsElement, wordlist_name);
+        }
+    }
+}
+
+function addWordlist(wordlistsElement, wordlist_name) {
+    wordlistsElement.innerHTML += ('<li>' + wordlist_name + '<\li>');
 }
